f242e3cebed06e740058dc45b70f8cbf8ae5a60a
saluspopuli <andrecortez539@gmail.com>
2023-11-25 16:44:29 +0800

    Update 1_Basics.py

--- Pygame Practice/1_Basics.py
+++ Pygame Practice/1_Basics.py
@@ -4,3 +4,16 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
+
+
+
+# Main game loop
+running = True
+while running:
+    
+    # Ensures that all events in the game are run
+    for event in pygame.event.get():
+        
+        # Quits game if pygame detects the quit event type
+        if event.type == pygame.QUIT:
+            running = False
